b46a5ed1023ced27383968bd71e46ed3d5057c9d
chandra20500 <50678178+chandra20500@users.noreply.github.com>
2019-11-13 18:23:36 +0530

    wild fire

--- insertdata.php
+++ insertdata.php
@@ -4,7 +4,7 @@
   mysqli_select_db($con,'wildfire');
 
   if(!$con){
-    echo "sed lyf";
+    echo "database not connected";
   }
 
   extract($_POST);
@@ -12,19 +12,31 @@
   $name = $_POST['name'];
   $teamname = $_POST['team-name'];
   $number = $_POST['number'];
-  $email = $_POST['email'];
+  $member = $_POST['member1']; 
+  $member = $_POST['member2'];
+  $member = $_POST['member3'];
+  $member = $_POST['member4'];
+  $member = $_POST['member5'];
+  $member = $_POST['member6'];
+  $member = $_POST['member7'];
+  $member = $_POST['member8'];
+  $member = $_POST['member9'];
+  $member = $_POST['member10'];
+  $email =  $_POST['email'];
   $institute = $_POST['institute'];
   $size = $_POST['size'];
   $city = $_POST['city'];
   $url = $_POST['url'];
 
-  $q = "INSERT INTO `teamdata`(`name`, `teamname`, `mobile_number`, `email`, `institute`, `size`, `city`, `url`) VALUES ('$name','$teamname','$number','$email','$institute','$size','$city','$url')";
+  $q = "INSERT INTO `teamdata`(`name`, `teamname`, `member1`, `member2`, `member3`, `member4`, `member5`, `member6`, `member7`, `member8`, `member9`, `member10`, `mobile_number`, `email`, `institute`, `size`, `city`, `url`, `id`) VALUES ('$name','$teamname','$member1','$member2','$member3','$member4','$member5','$member6','$member7','$member8','$member9','$member10','$number','$email','$institute','$size','$city','$url')";
+
+  // $q = "INSERT INTO `teamdata`(`name`, `teamname`, `mobile_number`, `email`, `institute`, `size`, `city`, `url`) VALUES ('$name','$teamname','$number','$email','$institute','$size','$city','$url')";
 
   if(mysqli_query($con,$q)){
     echo "successfull";
   }
   else{
-    echo "sed lyf";
+    echo "DATA not entered ";
   }
 
 
